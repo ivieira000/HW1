ba633baba501d38cf3721e4eac84b883ab29d7cd
ivieira000 <70668992+ivieira000@users.noreply.github.com>
2020-09-02 18:25:32 -0400

    Delete HW1.R

--- HW1.R
+++
@@ -1,3 +0,0 @@
-sample(x = 1:6, size = 20, replace = TRUE)
-# This line of code allows R to generate 20 random outcomes from a sample containing 6 different variables (labelled as numerical characters from 1 to 6).
-# "Replace = TRUE" means that each number can be drawn more than once, simulating what rolling a die would be like. 
